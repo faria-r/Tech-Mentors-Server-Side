cb4c74cb5eae4cea4a4ced2362829c4476131006
faria-r <fariacse11@gmail.com>
2022-10-26 15:17:15 +0600

    dynamic APi Routes created for client side

--- index.js
+++ index.js
@@ -3,11 +3,28 @@ const app = express();
 const port = process.env.port || 5000;
 const cors = require('cors');
 const categories = require('./Data/Course_category.json');
+const courses = require('./Data/course.json')
 
 app.use(cors());
 
 
-app.get('/',(req,res)=>{
+app.get('/categories',(req,res)=>{
     res.send(categories)
 })
+app.get('/categories/:id',(req,res)=>{
+    const id = req.params.id;
+    const course = courses.find(course => course.category_id == id);
+    res.send(course)
+})
+//api for courses
+app.get('/courses',(req,res)=>{
+    res.send(courses)
+})
+app.get('/checkout/:id',(req,res)=>{
+    const id = req.params.id;
+    const checkoutPage = courses.find(course => course._id === id);
+    console.log(checkoutPage._id)
+    res.send(checkoutPage)
+})
+
 app.listen(port,()=>console.log('tech mentor server running on:',port))
